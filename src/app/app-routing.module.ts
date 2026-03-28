36e457dae5a0330b2cae3a8435f20828a811a14c
iyulonia <chaykovskayayIlona@gmail.com>
2026-03-28 07:58:56 +0000

    Подготовили файлы

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -33,7 +33,7 @@ import { ForgotPasswordComponent } from './forgot-password/forgot-password.compo
 import { GazonComponent } from './gazon/gazon.component';
 import { WaterTrackerComponent } from './water-tracker/water-tracker.component';
 import { FaceDetectorComponent } from './face-detect/face-detector.component';  // ИСПРАВЛЕНО
-
+import { WeatherComponent } from './weather/weather.component';
 const routes: Routes = [
   {
     path: '',
@@ -215,6 +215,10 @@ const routes: Routes = [
   {
     path: 'fashion-detail', 
     component: FashionDetailComponent
+  },
+  {
+    path: 'LaLa', 
+    component: WeatherComponent
   },
   {
     path: '**',
---
+++ src/app/weather/weather.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { WeatherPageRoutingModule } from './weather.routing.module';
+
+
+
+@NgModule({
+  declarations: [],
+  imports: [ 
+    WeatherPageRoutingModule ,
+    CommonModule
+  ]
+})
+export class WeatherModule { }
